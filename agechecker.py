a9f0eefcdc8807ad2d6b6e7ba0ce495396bbc2e1
StringShot <akanbi.daniel@outlook.com>
2026-05-29 15:02:19 +0100

    Update agechecker.py

--- agechecker.py
+++ agechecker.py
@@ -5,7 +5,10 @@ elif age > 10 and age < 20:
     print("You are a inbetween 10 and 20 years old")
     if age >10 and age < 18:
         print("You are a teenager")
+    print("You can enter the class")
 elif age > 0 and age < 10:
     print("You are a child")
+    print("You are too young to enter the class")
 else: 
     print("You are an adult")
+    print("You are too old to enter the class")
